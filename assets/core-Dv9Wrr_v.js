c505f1915ec776811d2f063a571d30c3c3ac067a
Sergio Gonzalez <mr.man@Sergios-iMac.local>
2026-06-26 13:15:38 -0600

    deploy: v0.1.0 - Rust upgrade: progress, timeouts, streaming, stats

---
+++ assets/core-Dv9Wrr_v.js
@@ -0,0 +1,1 @@
+async function o(r,e={},n){return window.__TAURI_INTERNALS__.invoke(r,e,n)}function s(r,e="asset"){return window.__TAURI_INTERNALS__.convertFileSrc(r,e)}export{s as convertFileSrc,o as invoke};
---
+++ assets/index-BQvYfuyv.js
@@ -0,0 +1,1 @@
+import{invoke as n}from"./core-Dv9Wrr_v.js";async function r(e={}){return typeof e=="object"&&Object.freeze(e),await n("plugin:dialog|open",{options:e})}export{r as open};
